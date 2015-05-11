8b62e556f4e218f63c5875caff36f7edeaca5721
Daniel Berger <dberger@redhat.com>
2015-05-11 09:52:37 -0600

    Move subscription class into its own file.

---
+++ lib/azure/profile/subscription.rb
@@ -0,0 +1,16 @@
+class Subscription
+  attr_accessor :subscription_id
+  attr_accessor :subscription_name
+  attr_accessor :management_certificate
+  attr_accessor :default
+  attr_accessor :registered_providers
+  attr_accessor :environment_name
+  attr_accessor :management_endpoint
+  attr_accessor :source
+
+  DEFAULT_MANAGEMENT_ENDPOINT = "https://management.core.windows.net"
+
+  def initialize
+    yield self if block_given?
+  end
+end
