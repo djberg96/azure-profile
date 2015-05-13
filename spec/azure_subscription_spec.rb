84a52f8bdc37534bfde7161fba9931d0123dcd09
Daniel Berger <dberger@redhat.com>
2015-05-13 10:39:23 -0600

    Added tests for the rest of the Subscription accessors.

--- spec/azure_subscription_spec.rb
+++ spec/azure_subscription_spec.rb
@@ -7,23 +7,59 @@ require 'rspec/autorun'
 require 'azure/profile'
 
 describe "subscription" do
+  before(:all) do
+    @default_endpoint = "https://management.core.windows.net"
+  end
+
   before(:each) do
     @sub = Azure::Profile::Subscription.new
   end
 
   it "has a subscription_id accessor" do
     @sub.should respond_to(:subscription_id)
+    @sub.subscription_id.should be_nil
   end
 
   it "has a subscription_name accessor" do
     @sub.should respond_to(:subscription_name)
+    @sub.subscription_name.should be_nil
   end
 
   it "has a management_certificate accessor" do
     @sub.should respond_to(:management_certificate)
+    @sub.management_certificate.should be_nil
+  end
+
+  it "has a default accessor that defaults to false" do
+    @sub.should respond_to(:default)
+    @sub.default.should be_false
+  end
+
+  it "has a registered_providers accessor" do
+    @sub.should respond_to(:registered_providers)
+    @sub.registered_providers.should be_nil
+  end
+
+  it "has an environment_name accessor with a default value" do
+    @sub.should respond_to(:environment_name)
+    @sub.environment_name.should eq("AzureCloud")
+  end
+
+  it "has a management_endpoint accessor with a default value" do
+    @sub.should respond_to(:management_endpoint)
+    @sub.management_endpoint.should eq(@default_endpoint)
+  end
+
+  it "has a source accessor with a default value" do
+    @sub.should respond_to(:source)
+    @sub.source.should eq("ruby")
   end
 
   after(:each) do
     @sub = nil
+  end
+
+  after(:all) do
+    @default_endpoint = nil
   end
 end
