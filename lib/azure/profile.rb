505a39a7ae1914c322eb8dae72957edc20b53ff0
Daniel Berger <dberger@redhat.com>
2015-05-13 14:32:11 -0600

    Added VERSION constant.

--- lib/azure/profile.rb
+++ lib/azure/profile.rb
@@ -2,6 +2,9 @@ require_relative 'profile/subscription'
 
 module Azure
   class Profile
+    # The version of the azure-profile library
+    VERSION = '1.0.0'
+
     # The raw content of your publishsettings file
     attr_accessor :content
 
