3cc405b9c72cc81105613b015e578c8d669c8d68
Daniel Berger <dberger@redhat.com>
2016-04-10 06:12:39 -0600

    Bumped VERSION.

--- lib/azure/profile.rb
+++ lib/azure/profile.rb
@@ -3,7 +3,7 @@ require_relative 'profile/subscription'
 module Azure
   class Profile
     # The version of the azure-profile library
-    VERSION = '1.0.1'
+    VERSION = '1.0.2'
 
     # The raw content of your publishsettings file
     attr_accessor :content
